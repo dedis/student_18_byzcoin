455b8f4320144cc00e56fbb3da230d63dc450ff8
Pablo Lorenceau <pablo.lorenceau@bluewin.ch>
2018-04-23 22:31:13 +0200

    adding and passing testcase

--- omniledger/service/struct.go
+++ omniledger/service/struct.go
@@ -57,21 +57,24 @@ func (c *collectionDB) loadAll() {
 	})
 }
 
-// tryHash returns the merkle root of the collection as if the key value pair
-// had been added, without actually adding it.
+// tryHash returns the merkle root of the collection as if the key value pairs
+// in the transactions had been added, without actually adding it.
 func (c *collectionDB) tryHash(ts []Transaction) (mr []byte, rerr error) {
 	for _, t := range ts {
 		err := c.coll.Add(t.Key, t.Value, t.Kind)
 		if err != nil {
 			rerr = err
 			return
+		} else {
+			// remove the pair after we got the merkle root.
+			defer func(k []byte) {
+				err = c.coll.Remove(k)
+				if err != nil {
+					rerr = err
+					mr = nil
+				}
+			}(t.Key)
 		}
-		defer func() {
-			if err = c.coll.Remove(t.Key); err != nil {
-				rerr = err
-				mr = nil
-			}
-		}()
 	}
 	mr = c.coll.GetRoot()
 	return
